39f4339e1f419a987456a23c9b81c3b5f66a83d6
Tawana Kwaramba <tawnakwaramba@gmail.com>
2022-07-29 16:29:48 +0800

    simple fall detection, using the distance of the head from the hips of the resident

--- fallen_analyser/pose_fallen.py
+++ fallen_analyser/pose_fallen.py
@@ -3,6 +3,9 @@ import numpy as np
 import cv2
 
 VISIBILITY_THRESHOLD = 0.4
+#FALEN_DISTANCE_THRESHOLD = 240
+#FALEN_DISTANCE_THRESHOLD = 300
+FALEN_DISTANCE_THRESHOLD = 350
 
 class PoseFallen():
 
@@ -35,6 +38,7 @@ class PoseFallen():
                             self.mpDrawing.DrawingSpec(color=(66,117,245), thickness=2, circle_radius=2),
                             self.mpDrawing.DrawingSpec(color=(230,66,245), thickness=2, circle_radius=2))
 
+
         return results, cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
     def torsoInView(self, results):
@@ -42,12 +46,11 @@ class PoseFallen():
         """
         inView = False
 
-        #landmark = results.pose_landmarks.landmark
+        landmarkFound = results.pose_landmarks
 
-        landmark = results.pose_landmarks
-
-        if landmark:
-            landmark = results.pose_landmarks.landmark
+        #checking if any landmarks are found in the image
+        if landmarkFound:
+            landmark = landmarkFound.landmark
             leftShoulder = landmark[self.mpHolistic.PoseLandmark.LEFT_SHOULDER.value].visibility
             rightShoulder = landmark[self.mpHolistic.PoseLandmark.RIGHT_SHOULDER.value].visibility
             leftHip = landmark[self.mpHolistic.PoseLandmark.LEFT_HIP.value].visibility
@@ -60,4 +63,73 @@ class PoseFallen():
 
         return inView
 
+    def noseHipInView(self, results):
+        """
+        """
+        inView = False
 
+        landMarkFound = results.pose_landmarks
+
+        if landMarkFound: 
+            landmark = landMarkFound.landmark
+            nose = landmark[self.mpHolistic.PoseLandmark.NOSE.value].visibility
+            hip = landmark[self.mpHolistic.PoseLandmark.RIGHT_HIP.value].visibility
+
+            if (nose >= VISIBILITY_THRESHOLD) & (hip >= VISIBILITY_THRESHOLD):
+                inView = True
+
+        return inView
+
+
+    def orientationOfTorso(self, results):
+        """
+        """
+        orientation = None
+
+        landmark = results.pose_landmarks
+
+        #checking if any landmarks are found in the image
+        if landmark:
+            landmark = landmark.landmark
+
+        return orientation
+
+
+    def getDistanceHeadKnee(self, results, imgShape):
+        """
+        """
+
+
+        distance = None
+        landmarkFound = results.pose_landmarks
+
+        #checking  if any landmarks are found in the image 
+        if landmarkFound:
+            landmark = landmarkFound.landmark
+            head = landmark[self.mpHolistic.PoseLandmark.NOSE.value]
+            #we're going to assume that the left and right hip are going to be
+            #mediapipe returns width and height numbers between [0,1]. Therefore,
+            #normalising width and height to size of image
+            headHeight = head.y * imgShape[0]
+
+            #will only need to get distance of head, as measurements in 
+            #mediapipe are relative to the hip joints
+            distance = np.sqrt(pow(headHeight,2))
+
+        return distance
+
+    def hasFallenDistance(self, distance):
+        """
+        """
+
+        fallen = False
+        if distance >= FALEN_DISTANCE_THRESHOLD:
+            fallen = True
+
+
+        return fallen
+
+
+    def hasFallenOrientation(self, orientation):
+        """
+        """
--- fallen_analyser/pose_fallen_test.py
+++ fallen_analyser/pose_fallen_test.py
@@ -1,53 +1,82 @@
 from pose_fallen import PoseFallen
 import cv2
 
-keepOpen = True
-cap = cv2.VideoCapture(0)
-testDataPath = "test_data/SetElderly/"
-
-poseFallen = PoseFallen()
-
-"""
-#reading in an image from the test data 
-image = cv2.imread(testDataPath + "img_002.png")
-
-results, image = poseFallen.findPose(image)
-inView = poseFallen.torsoInView(results)
-
-if inView:
-    print("person's torso is in view")
-else:
-    print("person torso is NOT in view")
-
-cv2.imshow("fallen resident", image)
+def main():
+    #testImage()
+    testVideo()
 
 
-#waiting for a key to be pressed before closing down
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-"""
+def testImage():
+    poseFallen = PoseFallen()
+    testDataPath = "test_data/SetElderly/"
+    #reading in an image from the test data 
+    image = cv2.imread(testDataPath + "img_058.png") #image 2 and 58 are going to be good for testing this
+    image = cv2.imread(testDataPath + "img_002.png")
 
-while cap.isOpened and keepOpen:
-    _,frame = cap.read()
-    results, img = poseFallen.findPose(frame)
+    results, image = poseFallen.findPose(image)
+    inView = poseFallen.noseHipInView(results)
+    distance = poseFallen.getDistanceHeadKnee(results, image.shape)
+    fallen = poseFallen.hasFallenDistance(distance)
+    print("Euclidean distance ", distance)
 
-    inView = poseFallen.torsoInView(results)
-
-    color = None
     if inView:
-        text = "Person is in view"
-        color = (0,255,0)
+        print("person's torso is in view")
     else:
-        text = "Person is not in view"
-        color = (0,0,255)
+        print("person torso is NOT in view")
 
-    img = cv2.putText(img, text, (100,100), cv2.FONT_HERSHEY_SIMPLEX,1, color,
-            2, cv2.LINE_AA)
+    print("has fallen ", fallen)
 
-    cv2.imshow("Testing life vide feed", img)
+    cv2.imshow("fallen resident", image)
 
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        keepOpen = False
 
-cap.release()
-cv2.destroyAllWindows()
+    #waiting for a key to be pressed before closing down
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def testVideo():
+    keepOpen = True
+    cap = cv2.VideoCapture(0)
+    poseFallen = PoseFallen()
+    while cap.isOpened and keepOpen:
+        _,frame = cap.read()
+        results, img = poseFallen.findPose(frame)
+
+        """
+        #seeing if it has found any land marks
+        if results.pose_landmarks:
+            print(results.pose_landmarks.landmark[0])
+        """
+
+        inView = poseFallen.torsoInView(results)
+
+        color = None
+        text = ""
+
+        if inView:
+            text = "Person is in view"
+            color = (0,255,0)
+            distance = poseFallen.getDistanceHeadKnee(results, img.shape)
+            fallen = poseFallen.hasFallenDistance(distance)
+
+            if fallen:
+                text = "Fallen Resident"
+                color = (0, 0, 255)
+
+        img = cv2.putText(img, text, (100,100), cv2.FONT_HERSHEY_SIMPLEX,1, color,
+                2, cv2.LINE_AA)
+
+        cv2.imshow("Testing life vide feed", img)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            keepOpen = False
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def segmentImages():
+    """
+    to iterate over the setElderly data, make folders for fallen, and not fallen
+    """
+
+if __name__ == "__main__":
+    main()
