ad6ce79c7b20f7bcd8337d01de99ec469c3f8bed
Tawana Kwaramba <tawnakwaramba@gmail.com>
2022-08-29 23:03:02 +0800

    Fixed some naming conventions in MiRoVoice.py

--- src/miro/scripts/MiRoVoice.py
+++ src/miro/scripts/MiRoVoice.py
@@ -4,8 +4,9 @@ import numpy as np
 import time
 import sys
 import os
-#TODO: you will need to find out what this is actually going to do in code
-import hashlib
+
+#TODO: you will need to make the naming conventions here all consistent with each other
+#TODO: you will need to get rid of depreciated method from the program and file 
 
 #going to hard code the path to the signals which MiRo will require
 HELP_SIGNAL_FILE = "test_signal.mp3"
@@ -14,85 +15,48 @@ BUFFER_STUFF_BYTES = 4000
 MAX_STREAM_MSG_SIZE = (4096 - 48)
 #this is taken from the mdk/bin/shared/client_stream.py file 
 class Streamer():
-
     def __init__(self):
         #loading in the wav file into memory
         fileName = "/tmp/" + HELP_SIGNAL_FILE + ".decode"
 
-        #TODO: You will need to have a look at what that os.path stuff is going to do
-
-
         #loading the wave file into the program 
-        with open(fileName, "rb") as inStrm:
-            dat = inStrm.read()
-        self.data_r = 0
-
+        dat = self.readBinary(fileName)
+        self.dataR = 0
         # convert to numpy array
         dat = np.fromstring(dat, dtype='int16').astype(np.int32)
-
         # normalise wav
         dat = dat.astype(np.float)
         sc = 32767.0 / np.max(np.abs(dat))
         dat *= sc
         dat = dat.astype(np.int16).tolist()
 
-        # breakdown of digit file is recovered manually
-
-
-
-        """
-        # handle digits
-        if digits:
-
-            # start with some blank so we don't miss beginning
-            dat_ = dat
-            dat = [dat_[0] * 0] * 8000
-            gap = [dat_[0] * 0] * 50
-
-            # now add the digits
-            for d in digits:
-                if d in "0123456789":
-                    s = digits_and_dot[int(d) - int("0")]
-                    dat += dat_[s[0]:s[1]]
-                    dat += gap
-                if d == ".":
-                    s = digits_and_dot[10]
-                    dat += dat_[s[0]:s[1]]
-                    dat += gap
-        """
-
         # store
         self.data = dat
 
         # state
-        self.buffer_space = 0
-        self.buffer_total = 0
+        self.bufferSpace = 0
+        self.bufferTotal = 0
 
         # get robot name
-        topic_base_name = "/" + os.getenv("MIRO_ROBOT_NAME")
+        topicBaseName = "/" + os.getenv("MIRO_ROBOT_NAME")
 
         # publish
-        topic = topic_base_name + "/control/stream"
-        print ("publish", topic)
-        self.pub_stream = rospy.Publisher(topic, Int16MultiArray, queue_size=0)
+        topic = topicBaseName + "/control/stream"
+        self.pubStream = rospy.Publisher(topic, Int16MultiArray, queue_size=0)
 
-        #I am not going to worry about this at the current moment, I will come back to it later
-        # subscribe
-        topic = topic_base_name + "/platform/log"
-        print ("subscribe", topic)
-        self.sub_log = rospy.Subscriber(topic, String, self.callback_log, queue_size=5, tcp_nodelay=True)
+        topic = topicBaseName + "/platform/log"
+        self.subLog = rospy.Subscriber(topic, String, self.callback_log, queue_size=5, tcp_nodelay=True)
 
-        # subscribe
-        topic = topic_base_name + "/sensors/stream"
-        print ("subscribe", topic)
-        self.sub_stream = rospy.Subscriber(topic, UInt16MultiArray, self.callback_stream, queue_size=1, tcp_nodelay=True)
+        topic = topicBaseName + "/sensors/stream"
+        self.subStream = rospy.Subscriber(topic, UInt16MultiArray,
+                self.callback_stream, queue_size=1, tcp_nodelay=True)
 
     def playSound(self):
         count = 0
         exit = False
 
         # safety dropout if receiver not present
-        dropout_data_r = -1
+        dropout_dataR = -1
         dropout_count = 3
 
         # loop
@@ -100,35 +64,35 @@ class Streamer():
             if not os.path.isfile(HELP_SIGNAL_FILE):
                 exit = True
             # if we've received a report
-            if self.buffer_total > 0:
+            if self.bufferTotal > 0:
                 # compute amount to send
-                buffer_rem = self.buffer_total - self.buffer_space
+                buffer_rem = self.bufferTotal - self.bufferSpace
                 n_bytes = BUFFER_STUFF_BYTES - buffer_rem
                 n_bytes = max(n_bytes, 0)
                 n_bytes = min(n_bytes, MAX_STREAM_MSG_SIZE)
                 # if amount to send is non-zero
                 if n_bytes > 0:
-                    msg = Int16MultiArray(data = self.data[self.data_r:self.data_r+n_bytes])
-                    self.pub_stream.publish(msg)
-                    self.data_r += n_bytes
+                    msg = Int16MultiArray(data = self.data[self.dataR:self.dataR+n_bytes])
+                    self.pubStream.publish(msg)
+                    self.dataR += n_bytes
             # break
-            if self.data_r >= len(self.data):
+            if self.dataR >= len(self.data):
                 exit = True
 
             # report once per second
             if count == 0:
                 count = 10
-                print ("streaming:", self.data_r, "/", len(self.data), "bytes")
+                print ("streaming:", self.dataR, "/", len(self.data), "bytes")
 
                 # check at those moments if we are making progress, also
-                if dropout_data_r == self.data_r:
+                if dropout_dataR == self.dataR:
                     if dropout_count == 0:
                         print ("dropping out because of no progress...")
                         exit = True 
                     print ("dropping out in", str(dropout_count) + "...")
                     dropout_count -= 1
                 else:
-                    dropout_data_r = self.data_r
+                    dropout_dataR = self.dataR
 
             # count tenths
             count -= 1
@@ -139,8 +103,16 @@ class Streamer():
         sys.stdout.flush()
 
     def callback_stream(self, msg):
-        self.buffer_space = msg.data[0]
-        self.buffer_total = msg.data[1]
+        self.bufferSpace = msg.data[0]
+        self.bufferTotal = msg.data[1]
+
+    def readBinary(self, fileName):
+        dat = None
+        with open(fileName, "rb") as inStrm:
+            dat = inStrm.read()
+
+        return dat
+
 
 if __name__ == "__main__":
     rospy.init_node("test_node", anonymous=True)
